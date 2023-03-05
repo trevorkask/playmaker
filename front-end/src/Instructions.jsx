91778b3a77cef36fdbe31cbeb19625bf8169b353
Trevor Kasuku <trevor.p.kasuku@vanderbilt.edu>
2023-03-05 20:11:52 +0100

    Added all relevant files

---
+++ front-end/src/Instructions.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Instructions = () => {
+    return(
+        <div className="instructions-container">
+            <div className="user-stats"></div>
+            <div className="ad-2"></div>
+        </div>
+    )
+}
+
+export default Instructions;
---
+++ front-end/src/Stats.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Stats = () => {
+    return(
+        <div className="stats-container">
+            <div className="instructions-demo"></div>
+            <div className="ad-1"></div>
+        </div>
+    )
+}
+
+export default Stats;
