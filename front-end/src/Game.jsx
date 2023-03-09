c3bf8e048f1a268107ae53eb4eed0f1d0fb2a3b9
Trevor Kasuku <trevor.p.kasuku@vanderbilt.edu>
2023-03-09 16:14:07 +0100

    Added a search bar to the design

--- front-end/src/Game.jsx
+++ front-end/src/Game.jsx
@@ -3,7 +3,10 @@ import './Game.css';
 
 const Game = () => {
     return(
-        <div></div>
+        <div>
+            <p>Connect between these two players using mutual teammates</p>
+            <input type="text" placeholder="Trial 1 out of 5"></input>
+        </div>
     )
 }
 
