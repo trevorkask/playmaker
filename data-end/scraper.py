b81c8dad17b4dd7b96b2a48d3b767db5fa2251a8
Trevor Kasuku <trevor.p.kasuku@vanderbilt.edu>
2023-03-21 10:08:28 +0100

    Added data-end folder

---
+++ data-end/scraper.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://fbref.com/en/squads/76ffc013/2009-2010/Portsmouth-Stats'
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
+
+# Find the table that contains the player data for the relevant years
+table = soup.find('table', {'id': 'stats_standard_9'})
+
+# Iterate through each row of the table and extract the player names
+player_names = []
+for row in table.find_all('tr')[2:]:
+    cells = row.find_all('th')
+    name = cells[0].get_text()
+    player_names.append(name)
+
+# Repeat the above steps for each year of interest (2000-2023)
+# and store the player names in a list or a file for further analysis
+print(player_names)
